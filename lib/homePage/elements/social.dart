a4bae2b2d156e8914526100d6841e652ad311acf
dacian73 <dacian73@gmail.com>
2022-09-11 13:07:49 +0300

    small improvements / colors updated

--- lib/homePage/elements/social.dart
+++ lib/homePage/elements/social.dart
@@ -1,7 +1,7 @@
 import 'package:derzelas/logic/menu_controler.dart';
 import 'package:flutter/material.dart';
 import 'package:derzelas/homePage/colors.dart';
-
+import 'dart:html' as html;
 
 class Social extends StatelessWidget {
   const Social({
@@ -14,14 +14,40 @@ class Social extends StatelessWidget {
       children: [
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: smallPadding),
-          child: Container(height: 40,child: Image.asset('assets/img/fb.png', fit: BoxFit.fitHeight,), ),
+          child: Container(
+            height: 40,
+            child: InkWell(
+              child: Image.asset(
+                'assets/img/fb.png',
+                fit: BoxFit.fitHeight,
+                color: turcoaz,
+              ),
+              onTap: () {
+                html.window.open(
+                    'https://www.facebook.com/asociatiaderzelas', '_blank');
+              },
+            ),
+          ),
         ),
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: smallPadding),
-          child: Container(height: 40,child: Image.asset('assets/img/contact.png', fit: BoxFit.fitHeight,), ),
+          child: Container(
+            height: 40,
+            child: Image.asset(
+              'assets/img/contact.png',
+              fit: BoxFit.fitHeight,
+              color: turcoaz,
+            ),
+          ),
         ),
-        ElevatedButton(onPressed: (){}, child: Text('Îndrăznește și tu!'), style: TextButton.styleFrom(padding: EdgeInsets.symmetric(horizontal: largePadding, vertical: smallPadding)),),
-
+        ElevatedButton(
+          onPressed: () {},
+          child: Text('Îndrăznește și tu!'),
+          style: TextButton.styleFrom(
+              padding: EdgeInsets.symmetric(
+                  horizontal: largePadding, vertical: smallPadding),
+              backgroundColor: roz),
+        ),
       ],
     );
   }
