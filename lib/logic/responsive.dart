038e8c80b41b4f4229f231e7f39f30dd8e9913b8
dacian73 <dacian73@gmail.com>
2022-12-27 10:03:07 +0200

    Update responsive.dart

--- lib/logic/responsive.dart
+++ lib/logic/responsive.dart
@@ -12,8 +12,6 @@ class Responsive extends StatelessWidget {
     required this.desktop,
   }) : super(key: key);
 
-// This size work fine on my design, maybe you need some customization depends on your design
-
   static bool isMobile(BuildContext context) =>
       MediaQuery.of(context).size.width < 650;
 
@@ -27,16 +25,13 @@ class Responsive extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
-      // If our width is more than 900 then we consider it a desktop
       builder: (context, constraints) {
         if (constraints.maxWidth >= 1000) {
           return desktop;
         }
-        // If width it less then 900 and more then 650 we consider it as tablet
         else if (constraints.maxWidth >= 650) {
           return tablet;
         }
-        // Or less then that we called it mobile
         else {
           return mobile;
         }
